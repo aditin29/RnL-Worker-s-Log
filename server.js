86ac3fbe6b7766a4adedd35cde1e1d4068c49260
Aditi <aditin29@gmail.com>
2021-08-27 11:41:17 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -34,6 +35,16 @@ app.use('/sites', sitesRouter);
 //admin
 app.use('/admin', adminRouter);
 app.use('/auth', authRouter);
+
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 
 app.listen(port, () => {
